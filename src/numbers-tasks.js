871daa8fbc1c92e94450eaa9e974871d0f3de5ac
KVirB <dlaydrygaakk@mail.ru>
2023-11-17 21:21:38 +0300

    feat: fix test

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -581,7 +581,7 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -1, 1 => -1 | 0 | 1
  */
 function getRandomInteger(min, max) {
-  return Math.random() * (max - min) + min;
+  return Number.parseInt(Math.random() * (max - min) + min, 10);
 }
 
 /**
